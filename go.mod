16229e971ab24d38fdfaeef980728916479433f9
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-02-17 21:32:19 -0500

    added handler POST /items/create for creating items

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goItemManagementApi
 
 go 1.21.6
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 // Item struct represents an item with ID and Name
@@ -16,8 +18,8 @@ var items = []Item{} // In-memory storage for items
 const Dport = ":8000"
 
 func main() {
-	fmt.Println(items)
 	http.HandleFunc("/items", getItemsHandler)
+	http.HandleFunc("/items/create", addItemHandler)
 
 	fmt.Printf("Server is starting on port: %v\n", Dport) // Added newline for better terminal output
 	http.ListenAndServe(Dport, nil)
@@ -27,4 +29,21 @@ func main() {
 func getItemsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(items)
+}
+
+// this handler adds a new item to the collection
+func addItemHandler(w http.ResponseWriter, r *http.Request) {
+	var newItem Item
+	err := json.NewDecoder(r.Body).Decode(&newItem)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Generate an unique ID for the new item 
+	newItem.ID = uuid.New().String()
+	items = append(items, newItem)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newItem)
 }
